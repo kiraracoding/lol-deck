1a61f10671705aafabb25e5daeb07b8d8235456c
kiraracoding <merandilla.alessandra1121@gmail.com>
2025-10-04 14:04:59 +0200

    my champions page setup

--- src/pages/MyChampionsPage.jsx
+++ src/pages/MyChampionsPage.jsx
@@ -4,7 +4,26 @@ function MyChampionsPage() {
     
     return(
         <>
-            <p>this is my champions page</p>
+        <div className="pt-[110px] h-screen w-full flex flex-row">
+            
+            <div className="basis-1/4 bg-gray-300 flex flex-col items-center pt-[50px]">
+                <div className="flex flex-col items-center w-[200px]">
+                    <h1>Champions Learned:</h1>
+                    <p>0</p>
+                </div>
+
+                <div className="flex flex-col items-center w-[200px] pt-[30px]">
+                    <h1>Champions Left:</h1>
+                    <p>0</p>
+                </div>
+            </div>
+
+            <div className="basis-3/4 bg-gray-500">
+                <p>this is learned section</p>
+            </div>
+
+        </div>
+            
         </>
     )
 }
