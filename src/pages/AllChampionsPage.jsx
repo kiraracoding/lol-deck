a4266c7d30e90b1feaf89b6f3a5c2532c5388028
kiraracoding <merandilla.alessandra1121@gmail.com>
2025-10-07 19:33:01 +0200

    All Champions Page and My Champions Page Layout Complete

--- src/pages/AllChampionsPage.jsx
+++ src/pages/AllChampionsPage.jsx
@@ -4,11 +4,11 @@ function AllChampionsPage() {
     
     return(
         <>
-            <div className="flex flex-row pt-[110px] 
+            <div className="flex flex-row pt-[95px]
                             w-full min-h-screen
                             min-w-[375px]">
             
-                <div className="hidden
+                <div className="hidden 
                                 2xl:flex flex-col items-center
                                 2xl:basis-1/5 bg-gray-300 pt-20
                                 xl:flex flex-col items-center
@@ -19,9 +19,9 @@ function AllChampionsPage() {
                                 md:basis-1/5 bg-gray-300 pt-20
                                 sm:flex flex-col items-center
                                 sm:basis-1/5 bg-gray-300 pt-20
-                                ">
+                                "> 
 
-                    <div className="flex flex-col items-center w-[200px]">
+                    <div className="flex flex-col items-center w-[200px] pt-[15px]">
                         <h1>Champions Learned:</h1>
                         <p>0</p>
                     </div>
@@ -41,6 +41,15 @@ function AllChampionsPage() {
                                 md:basis-4/5
                                 sm:basis-4/5
                                 ">
+                    <div className='text-center pt-15
+                                    2xl:hidden
+                                    xl:hidden
+                                    lg:hidden
+                                    md:hidden
+                                    sm:hidden'>
+                        <h1>Champions Learned: 0</h1>
+                        <h1>Champions Left: 0</h1>
+                    </div>
                     <div className="w-8/9 mt-20">
                         <UnlearnedCard/>
                         <UnlearnedCard/>
--- src/pages/MyChampionsPage.jsx
+++ src/pages/MyChampionsPage.jsx
@@ -5,10 +5,19 @@ function MyChampionsPage() {
     return(
         <>
         <div className="flex flex-row pt-[110px] 
-                        w-full min-h-screen ">
+                        w-full min-h-screen min-w-[375px]">
             
-            <div className="flex flex-col items-center
-                            basis-1/5 bg-gray-300 pt-20">
+            <div className="hidden
+                            2xl:flex flex-col items-center
+                            2xl:basis-1/5 bg-gray-300 pt-20
+                            xl:flex flex-col items-center
+                            xl:basis-1/5 bg-gray-300 pt-20
+                            lg:flex flex-col items-center
+                            lg:basis-1/5 bg-gray-300 pt-20
+                            md:flex flex-col items-center
+                            md:basis-1/5 bg-gray-300 pt-20
+                            sm:hidden                                
+                            ">
                 <div className="flex flex-col items-center w-[200px]">
                     <h1>Champions Learned:</h1>
                     <p>0</p>
@@ -21,16 +30,39 @@ function MyChampionsPage() {
                 </div>
             </div>
 
-            <div className="basis-4/5 flex flex-col 
+            <div className="w-full flex flex-col bg-gray-500
                             justify-center items-center 
-                            bg-gray-500">
-                <div className='grid grid-cols-1 gap-10 
+                            2xl:basis-4/5
+                            xl:basis-4/5
+                            lg:basis-4/5
+                            md:basis-4/5
+                            sm:w-full
+                            ">
+                <div className='text-center pt-15
+                                2xl:hidden
+                                xl:hidden
+                                lg:hidden
+                                md:hidden
+                                sm:text-center'>
+                    <h1>Champions Learned: 0</h1>
+                    <h1>Champions Left: 0</h1>
+                </div>
+                <div className='grid grid-cols-1 gap-x-10 
                                 justify-items-center p-10
                                 w-67/68
-                                sm:grid-cols-1 
+                                sm:grid-cols-2 
                                 md:grid-cols-2 
-                                lg:grid-cols-3 
-                                xl:grid-cols-4'>
+                                lg:grid-cols-3
+                                xl:grid-cols-4
+                                2xl:grid-cols-5
+                                '
+                                >
+                    <LearnedCard/>
+                    <LearnedCard/>
+                    <LearnedCard/>
+                    <LearnedCard/>
+                    <LearnedCard/>
+                    <LearnedCard/>
                     <LearnedCard/>
                     <LearnedCard/>
                     <LearnedCard/>
